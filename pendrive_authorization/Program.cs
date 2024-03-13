31f0b58f9fa0b2a9f9a5f780d61e50c9ac32107c
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-03-13 19:22:40 +0100

    Creating a code template for improvement.

--- pendrive_authorization/Program.cs
+++ pendrive_authorization/Program.cs
@@ -2,9 +2,74 @@
 {
     internal class Program
     {
+        static string authorizationFileName = "authorization.txt";
+        static string authorizationPassword = "password";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            if (checkAuthorization())
+            {
+                Console.WriteLine("Authorization was successful...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Authorization failed...");
+                Console.ReadKey();
+            }
+        }
+
+        private static bool checkAuthorization()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                if(drive.DriveType == DriveType.Removable)
+                {
+                    if(findAuthorizationFile(drive.RootDirectory.FullName))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool findAuthorizationFile(string drivePath)
+        {
+            string filePath = Path.Combine(drivePath, authorizationFileName);
+
+            if(File.Exists(filePath))
+            {
+                if(loadAuthorizationFile(filePath))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool loadAuthorizationFile(string filePath)
+        {
+            try
+            {
+                string readedAuthorizationPassword = File.ReadAllText(filePath);
+
+                if(readedAuthorizationPassword == authorizationPassword)
+                {
+                    return true;
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+
+            return false;
         }
     }
 }
