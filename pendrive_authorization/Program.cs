9e267a094ac50022fe7922db74c0564fad8e3fea
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-03-13 19:00:30 +0100

    Add project files.

---
+++ pendrive_authorization/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pendrive_authorization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
